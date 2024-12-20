d9c440ab9be80bf8a4a517e41760f820ec870abc
Nick Franczak <nicholasgfranczak@gmail.com>
2024-12-20 13:31:09 -0500

    merge branch with main

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -6,58 +6,11 @@
 	import Controllers from './Controllers.svelte'
 	import Hands from './Hands.svelte'
 	import { useResources } from '$lib/hooks/useResources'
-	import type { Detection, VisionClient } from '@viamrobotics/sdk'
-	import { onDestroy } from 'svelte'
+	import Detections from './Detections.svelte'
 
 	const resources = useResources()
-	console.log("All resources: ", $resources)
-	
-	// Log each resource's details
-	$resources.forEach(r => {
-		console.log('Resource:', {
-			name: r.name,
-			type: r.type,
-			subtype: r.subtype
-		})
-	})
-	console.log("hello there 111")
 
 	const camResource = $derived($resources.filter((r) => r.subtype === 'camera')[0])
-	const visResource = $derived($resources.filter((r) => r.subtype === 'vision')[0]) as VisionClient
-
-	console.log('Filtered resources:', {
-		camera: camResource,
-		vision: visResource
-	})
-
-	let detections: Detection[] = []
-	let detectionInterval: number
-
-	async function getDetections() {
-		if (!visResource || !camResource?.name) {
-			console.log('Missing resources:', { visResource, camResource })
-			return
-		}
-		console.log('have resources!:', { visResource, camResource })
-		
-		try {
-			const result = await (visResource as VisionClient).getDetectionsFromCamera(camResource.name)
-			console.log("result: ", result)
-			detections = result
-			console.log('Detections:', detections)
-		} catch (error) {
-			console.error('Error getting detections:', error)
-		}
-	}
-
-	// Call getDetections periodically or based on your needs
-	detectionInterval = setInterval(getDetections, 1000) // Updates every second
-
-	onDestroy(() => {
-		if (detectionInterval) {
-			clearInterval(detectionInterval)
-		}
-	})
 
 	// $effect(() => console.log($resources))
 </script>
@@ -67,8 +20,6 @@
 	<Controllers />
 	<Detections />
 	<Hands />
-
-	<OriginMarker />
 </XR>
 
 <T.PerspectiveCamera
