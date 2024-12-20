ee38377ef57857a1831bd00250f34fc128a331e1
Abe Winter <abe-winter@users.noreply.github.com>
2024-12-20 13:55:48 -0500

    position for offsets

--- src/lib/components/DetectionsPlane.svelte
+++ src/lib/components/DetectionsPlane.svelte
@@ -4,8 +4,8 @@
 
 	export let detections: Detection[] = [
 		{
-			xMin: BigInt(0),
-			xMax: BigInt(1),
+			xMin: BigInt(1),
+			xMax: BigInt(2),
 			yMin: BigInt(0),
 			yMax: BigInt(2),
 			confidence: 0.5,
@@ -16,7 +16,10 @@
 
 <T.Group>
 	{#each detections as det}
-		<T.Mesh scale={[Number(det.xMax - det.xMin), Number(det.yMax - det.yMin), 1]}>
+		<T.Mesh
+			scale={[Number(det.xMax - det.xMin), Number(det.yMax - det.yMin), 1]}
+			position={[Number(det.xMin), Number(det.yMin), 0]}
+		>
 			<T.PlaneGeometry />
 		</T.Mesh>
 	{/each}
