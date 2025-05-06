b245fb93e34e79ce1463173b33e23bfd32fef821
mrloureed <mrloureed@gmail.com>
2025-05-06 11:12:45 -0400

    cleanup select object

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { useSelectedFrame, useFocusedFrame, useFocus } from '$lib/hooks/useSelection.svelte'
 	import { Check, Copy } from 'lucide-svelte'
-	import { Button, Icon } from '@viamrobotics/prime-core'
+	import { Button, IconButton } from '@viamrobotics/prime-core'
 
 	const selectedFrame = useSelectedFrame()
 	const focusedFrame = useFocusedFrame()
@@ -14,134 +14,121 @@
 {#if frame}
 	{@const { pose, name } = frame}
 	{@const { center, geometryType } = frame.geometry ?? {}}
-	<div class="border-medium bg-extralight fixed top-0 right-0 z-10 m-2 w-54 border p-2 text-xs">
-		<div class="flex items-center justify-between gap-2 pb-2">
-			<div class="flex items-center gap-1">
-				<button>
-					<Icon name="drag" />
-				</button>
+	<div class="border-medium bg-extralight fixed top-0 right-0 z-10 m-2 w-54 border text-xs">
+		<div class="border-medium flex items-center justify-between gap-2 border-b">
+			<div class="flex items-center">
+				<IconButton
+					icon="drag"
+					label="draggable icon"
+					variant="ghost"
+					cx="size-6"
+				/>
 				<strong class="font-semibold">{name}</strong>
 			</div>
 		</div>
 
-		<div class="border-medium -mx-2 w-[100%+0.5rem] border-b"></div>
+		<div class="p-3">
+			<h3 class="text-subtle-2 flex justify-between pb-2">
+				Details
 
-		<h3 class="text-subtle-2 flex justify-between py-2">
-			Details
+				<button
+					onclick={async () => {
+						navigator.clipboard.writeText(JSON.stringify($state.snapshot(frame)))
+						copied = true
+						setTimeout(() => (copied = false), 1000)
+					}}
+				>
+					{#if copied}
+						<Check size={14} />
+					{:else}
+						<Copy size={14} />
+					{/if}
+				</button>
+			</h3>
 
-			<button
-				onclick={async () => {
-					navigator.clipboard.writeText(JSON.stringify($state.snapshot(frame)))
-					copied = true
-					setTimeout(() => (copied = false), 1000)
-				}}
-			>
-				{#if copied}
-					<Check size={14} />
-				{:else}
-					<Copy size={14} />
-				{/if}
-			</button>
-		</h3>
-
-		<div class="flex flex-col gap-2.5">
-			{#if pose}
-				<div>
-					<strong class="font-semibold">position</strong>
-					<div class="flex gap-3">
-						<div>
-							<span class="text-subtle-2">x</span>
-							{pose.x.toFixed(2)}
-						</div>
-						<div>
-							<span class="text-subtle-2">y</span>
-							{pose.y.toFixed(2)}
-						</div>
-						<div>
-							<span class="text-subtle-2">z</span>
-							{pose.z.toFixed(2)}
-						</div>
-					</div>
-				</div>
-
-				<div>
-					<strong class="font-semibold">orientation</strong>
-					<div class="flex gap-3">
-						<div>
-							<span class="text-subtle-2">x</span>
-							{pose.oX.toFixed(2)}
-						</div>
-						<div>
-							<span class="text-subtle-2">y</span>
-							{pose.oY.toFixed(2)}
-						</div>
-						<div>
-							<span class="text-subtle-2">z</span>
-							{pose.oZ.toFixed(2)}
-						</div>
-						<div>
-							<span class="text-subtle-2">th</span>
-							{pose.theta.toFixed(2)}
-						</div>
-					</div>
-				</div>
-			{/if}
-
-			{#if center}
-				<div>
-					<strong class="font-semibold">center</strong>
-					<div class="flex gap-3">
-						<div>
-							<span class="text-subtle-2">x</span>
-							{center?.x.toFixed(2)}
-						</div>
-						<div>
-							<span class="text-subtle-2">y</span>
-							{center?.y.toFixed(2)}
-						</div>
-						<div>
-							<span class="text-subtle-2">z</span>
-							{center?.z.toFixed(2)}
-						</div>
-					</div>
-				</div>
-			{/if}
-
-			{#if geometryType}
-				{#if geometryType.case === 'box'}
+			<div class="flex flex-col gap-2.5">
+				{#if pose}
 					<div>
-						<strong class="font-semibold">dimensions</strong>
+						<strong class="font-semibold">position</strong>
 						<div class="flex gap-3">
 							<div>
 								<span class="text-subtle-2">x</span>
-								{geometryType.value.dimsMm?.x.toFixed(2)}
+								{pose.x.toFixed(2)}
 							</div>
 							<div>
 								<span class="text-subtle-2">y</span>
-								{geometryType.value.dimsMm?.y.toFixed(2)}
+								{pose.y.toFixed(2)}
 							</div>
 							<div>
 								<span class="text-subtle-2">z</span>
-								{geometryType.value.dimsMm?.z.toFixed(2)}
+								{pose.z.toFixed(2)}
 							</div>
 						</div>
 					</div>
-				{:else if geometryType.case === 'capsule'}
+
 					<div>
-						<strong class="font-semibold">dimensions</strong>
+						<strong class="font-semibold">orientation</strong>
 						<div class="flex gap-3">
 							<div>
-								<span class="text-subtle-2">r</span>
-								{geometryType.value.radiusMm.toFixed(2)}
+								<span class="text-subtle-2">x</span>
+								{pose.oX.toFixed(2)}
 							</div>
 							<div>
-								<span class="text-subtle-2">l</span>
-								{geometryType.value.lengthMm.toFixed(2)}
+								<span class="text-subtle-2">y</span>
+								{pose.oY.toFixed(2)}
+							</div>
+							<div>
+								<span class="text-subtle-2">z</span>
+								{pose.oZ.toFixed(2)}
+							</div>
+							<div>
+								<span class="text-subtle-2">th</span>
+								{pose.theta.toFixed(2)}
 							</div>
 						</div>
 					</div>
-				{:else if geometryType.case === 'sphere'}
-					<div class="flex justify-between">
+				{/if}
+
+				{#if center}
+					<div>
+						<strong class="font-semibold">center</strong>
+						<div class="flex gap-3">
+							<div>
+								<span class="text-subtle-2">x</span>
+								{center?.x.toFixed(2)}
+							</div>
+							<div>
+								<span class="text-subtle-2">y</span>
+								{center?.y.toFixed(2)}
+							</div>
+							<div>
+								<span class="text-subtle-2">z</span>
+								{center?.z.toFixed(2)}
+							</div>
+						</div>
+					</div>
+				{/if}
+
+				{#if geometryType}
+					{#if geometryType.case === 'box'}
+						<div>
+							<strong class="font-semibold">dimensions</strong>
+							<div class="flex gap-3">
+								<div>
+									<span class="text-subtle-2">x</span>
+									{geometryType.value.dimsMm?.x.toFixed(2)}
+								</div>
+								<div>
+									<span class="text-subtle-2">y</span>
+									{geometryType.value.dimsMm?.y.toFixed(2)}
+								</div>
+								<div>
+									<span class="text-subtle-2">z</span>
+									{geometryType.value.dimsMm?.z.toFixed(2)}
+								</div>
+							</div>
+						</div>
+					{:else if geometryType.case === 'capsule'}
 						<div>
 							<strong class="font-semibold">dimensions</strong>
 							<div class="flex gap-3">
@@ -149,32 +136,48 @@
 									<span class="text-subtle-2">r</span>
 									{geometryType.value.radiusMm.toFixed(2)}
 								</div>
+								<div>
+									<span class="text-subtle-2">l</span>
+									{geometryType.value.lengthMm.toFixed(2)}
+								</div>
 							</div>
 						</div>
-					</div>
+					{:else if geometryType.case === 'sphere'}
+						<div class="flex justify-between">
+							<div>
+								<strong class="font-semibold">dimensions</strong>
+								<div class="flex gap-3">
+									<div>
+										<span class="text-subtle-2">r</span>
+										{geometryType.value.radiusMm.toFixed(2)}
+									</div>
+								</div>
+							</div>
+						</div>
+					{/if}
 				{/if}
+			</div>
+
+			<h3 class="text-subtle-2 pt-3 pb-2">Actions</h3>
+
+			{#if focus.current}
+				<Button
+					class="w-full"
+					icon="arrow-left"
+					variant="dark"
+					onclick={() => focus.set(undefined)}
+				>
+					Exit object view
+				</Button>
+			{:else}
+				<Button
+					class="w-full"
+					icon="image-filter-center-focus"
+					onclick={() => focus.set(name)}
+				>
+					Enter object view
+				</Button>
 			{/if}
 		</div>
-
-		<h3 class="text-subtle-2 pt-3 pb-2">Actions</h3>
-
-		{#if focus.current}
-			<Button
-				class="w-full"
-				icon="arrow-left"
-				variant="dark"
-				onclick={() => focus.set(undefined)}
-			>
-				Exit object view
-			</Button>
-		{:else}
-			<Button
-				class="w-full"
-				icon="image-filter-center-focus"
-				onclick={() => focus.set(name)}
-			>
-				Enter object view
-			</Button>
-		{/if}
 	</div>
 {/if}
