e9e52f8fc0be84395d550b3b14deab2dbd122a6c
Micheal Parks <micheal.parks@outlook.com>
2026-05-04 13:10:41 -0400

    better reconnection logic and ui

--- eslint.config.js
+++ eslint.config.js
@@ -128,6 +128,7 @@ export default defineConfig(
 			'unicorn/prefer-top-level-await': 'off',
 			'unicorn/prevent-abbreviations': 'off',
 			'unicorn/require-module-specifiers': 'off',
+			'unicorn/prefer-global-this': 'off',
 
 			// TODO
 			// 'unicorn/filename-case': [
