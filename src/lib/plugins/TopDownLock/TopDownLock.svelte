07a0bf45bdababed893e1d238f2a86eadaea775a
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2026-06-18 11:58:18 -0400

    Sanding touchscreen fixes (#785)
    * sanding fixes
    
    * fixes
    
    * Create olive-regions-glow.md
    
    * cleanup

---
+++ src/lib/plugins/TopDownLock/TopDownLock.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { useCameraControls } from '$lib/hooks/useControls.svelte'
+
+	const controls = useCameraControls()
+
+	$effect(() => {
+		if (!controls.current) return
+
+		const currentControls = controls.current
+
+		if ('minPolarAngle' in currentControls) {
+			const { minPolarAngle, maxPolarAngle } = currentControls
+
+			// Locks the camera to top down while this component is mounted
+			currentControls.polarAngle = 0
+			currentControls.minPolarAngle = 0
+			currentControls.maxPolarAngle = 0
+
+			return () => {
+				currentControls.minPolarAngle = minPolarAngle
+				currentControls.maxPolarAngle = maxPolarAngle
+			}
+		}
+	})
+</script>
