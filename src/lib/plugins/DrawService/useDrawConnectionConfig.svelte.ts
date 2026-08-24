479c92599a3bda35de0ebcd6eedda9fa143a2a34
Devin C <devin@viam.com>
2026-08-24 11:41:20 -0400

    parallelize e2e tests

--- src/lib/plugins/DrawService/useDrawConnectionConfig.svelte.ts
+++ src/lib/plugins/DrawService/useDrawConnectionConfig.svelte.ts
@@ -1,8 +1,13 @@
 import { getContext, setContext } from 'svelte'
 
+export const DEFAULT_DRAW_SERVICE_PORT = '3030'
+
 export interface DrawConnectionConfig {
 	backendIP: string
 	websocketPort: string
+
+	/** Connect-RPC port for the draw service. Defaults to `DEFAULT_DRAW_SERVICE_PORT`. */
+	drawServicePort?: string
 }
 
 interface Context {
